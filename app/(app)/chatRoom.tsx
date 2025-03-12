4918fb120e8d6c2ab5d855fcb96f1dd34a54d2fb
nikitagts <nikitasnizko112@gmail.com>
2025-03-12 12:39:52 +0100

    added ChatRoom

--- app/(app)/chatRoom.tsx
+++ app/(app)/chatRoom.tsx
@@ -4,8 +4,9 @@ import {
   StatusBar,
   TextInput,
   TouchableOpacity,
+  Alert,
 } from "react-native";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import ChatRoomHeader from "@/components/ChatRoomHeader";
 import MessagesList from "@/components/MessagesList";
@@ -15,10 +16,86 @@ import {
 } from "react-native-responsive-screen";
 import { Feather } from "@expo/vector-icons";
 import CustomKeyBoardView from "@/components/CustomKeyBoardView";
+import { useAuth } from "@/context/authContext";
+import { getRoomID } from "@/utils/common";
+import {
+  addDoc,
+  collection,
+  doc,
+  DocumentData,
+  onSnapshot,
+  orderBy,
+  query,
+  setDoc,
+  Timestamp,
+} from "firebase/firestore";
+import { db } from "@/firebaseConfig";
 
 const ChatRoom = () => {
   const item = useLocalSearchParams();
+  const { user } = useAuth();
   const router = useRouter();
+  const [messages, setMessages] = useState<DocumentData[]>([]);
+  const textRef = useRef("");
+  const inputRef = useRef(null);
+
+  // console.log("ITEM:", item);
+  // console.log("USER:", user);
+
+  useEffect(() => {
+    createRoomIfNotExists();
+
+    let roomId = getRoomID(user?.uid, item?.userId);
+    const docRef = doc(db, "rooms", roomId);
+    const messageRef = collection(docRef, "messages");
+    const q = query(messageRef, orderBy("createdAt", "asc"));
+
+    const unsub = onSnapshot(q, (snapshot) => {
+      let allMessages = snapshot.docs.map((doc) => {
+        return doc.data();
+      });
+
+      setMessages([...allMessages]);
+    });
+    return unsub;
+  }, []);
+
+  const createRoomIfNotExists = async () => {
+    let roomId = getRoomID(user?.uid, item?.userId);
+
+    await setDoc(doc(db, "rooms", roomId), {
+      roomId,
+      createdAT: Timestamp.fromDate(new Date()),
+    });
+  };
+
+  const handleSetMessage = async () => {
+    let message = textRef.current.trim();
+    if (!message) return;
+
+    try {
+      let roomId = getRoomID(user?.uid, item?.userId);
+      const docRef = doc(db, "rooms", roomId);
+      const messageRef = collection(docRef, "messages");
+
+      textRef.current = "";
+      if (inputRef.current) (inputRef.current as TextInput).clear();
+
+      const newDoc = await addDoc(messageRef, {
+        userId: user?.uid,
+        text: message,
+        profileUrl: user?.profileUrl,
+        senderName: user?.userName,
+        createdAt: Timestamp.fromDate(new Date()),
+      });
+
+      console.log("newDoc:", newDoc.id);
+    } catch (error: any) {
+      Alert.alert("Error", error.message);
+    }
+  };
+
+  console.log("MESSAGES:", messages);
 
   return (
     <View className="flex-1 bg-white">
@@ -32,11 +109,14 @@ const ChatRoom = () => {
           {/* <View className="flex-row justify-between items-center mx-3"> */}
           <View className="flex-row justify-between bg-white border p-2 border-neutral-300 rounded-full pl-5 mx-2">
             <TextInput
+              ref={inputRef}
+              onChangeText={(text) => (textRef.current = text)}
               placeholder="Type a message..."
               style={{ fontSize: hp(2), height: hp(5) }}
               className="flex-1 mr-2 align-center"
             />
             <TouchableOpacity
+              onPress={handleSetMessage}
               style={{ width: hp(5) }}
               className="bg-neutral-200 p-3 rounded-full flex justify-center items-center"
             >
--- utils/common.ts
+++ utils/common.ts
@@ -1,2 +1,9 @@
 export const blurhash =
   "|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[";
+
+export const getRoomID = (id1: any, id2: any) => {
+  const sortedIds = [id1, id2].sort();
+
+  const roomId = sortedIds.join("-");
+  return roomId;
+};
